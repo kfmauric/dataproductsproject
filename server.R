f11b07a8d3bdd965331376aef3bb7025a1b1c844
kfmauric <kyle.maurice@gmail.com>
2015-12-22 09:13:56 -0800

    Initial version with map and civic apps api access.

--- server.R
+++ server.R
@@ -1,5 +1,20 @@
 library(shiny)
+library(leaflet)
+library(jsonlite)
 shinyServer(
-  function(input, output) {
-  }
+  function(input, output, session) {
+    #grab data from api and make location table
+    jsonData <- fromJSON("http://api.civicapps.org/restaurant-inspections?count=1000&since=2014-12-27")
+    descPoint <- cbind(jsonData$results$location[,c("Longitude", "Latitude")], jsonData$results[c("name", "score")])
+
+    descPoint$content <- paste(sep = "<br/>",
+                              descPoint$name, descPoint$score
+                )
+    waitIcon <- makeIcon("Food-Waiter-icon.png",24,24)
+    output$mymap <- renderLeaflet({
+      leaflet(data=descPoint) %>% setView(lat = 45.5200, lng = -122.6819, zoom = 12) %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        addMarkers(~Longitude, ~Latitude, popup=~content, clusterOptions = markerClusterOptions(maxClusterRadius=80, disableClusteringAtZoom = 17), icon=waitIcon)
+    })
+    }
 )
--- ui.R
+++ ui.R
@@ -1,10 +1,35 @@
 library(shiny)
-shinyUI(pageWithSidebar(
-  headerPanel("data science for the win"),
-  sidebarPanel(
-    h3('Sidebar text')
-  ),
-  mainPanel(
-    h3('Main Panel text')
+library(leaflet)
+shinyUI(fluidPage(
+  titlePanel("Portland Oregon Restaurant Inspection Reports"),
+  sidebarLayout( position = "left",
+    sidebarPanel(
+      h2("Overview"),
+      p("This application provides an interface to the restaurant inspection reports provided by the",a("civic apps API", href="http://api.civicapps.org/"), "in Portland Oregon"),
+      p("Restaurant Icon provided courstesy of",
+        a("Icon8", href="http://icons8.com"),"."),
+      br(),
+      br(),
+      br(),
+      br(),
+      img(src = "bigorb.png", height = 72, width = 72),
+      "shiny is a product of ",
+      span("RStudio", style = "color:blue")
+    ),
+    mainPanel(
+      h1("Introducing Shiny"),
+      p("Shiny is a new package from R studio that makes it", em("incredibly easy"), " to build interactive web application with R."),
+      br(),
+      p("For an introuction and live examples visit the",
+        a("Shiny homepage.",
+          href = "http://www.rstudio.com/shiny")),
+      br(),
+      h2("Features"),
+      p("*Build appplications with aonly a few linbes of code - no javasdcript required."),
+      p("*Shiny applications are automatically live in the same way that", strong("spreadsheets"), "are live. Outputs change instantly as users modify inputs, withoput requiring a reload of the browser."),
+      leafletOutput("mymap"),
+      p(),
+      actionButton("recalc", "New points")
+    )
   )
 ))
