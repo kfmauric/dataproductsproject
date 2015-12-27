81101f5faf642ec7b1a51b812ee37791c7895ade
kfmauric <kyle.maurice@gmail.com>
2015-12-27 12:44:40 -0800

    Initial submission with maping functions removed.

--- server.R
+++ server.R
@@ -8,14 +8,42 @@ shinyServer(
   #grab and process initial data
   function(input, output, session) {
     #grab data from api and make location table
-    jsonData <- fromJSON("http://api.civicapps.org/restaurant-inspections?count=1000&since=2014-12-27")
-    descPoint <- cbind(jsonData$results$location[,c("Longitude", "Latitude")], jsonData$results[c("name", "score")])
+    #big grab upfront and then subset in R
+    jsonData <- fromJSON("http://api.civicapps.org/restaurant-inspections?count=1000&since=2000-12-27")
+    descPoint <- cbind(jsonData$results$location[c("Longitude", "Latitude")],
+                       jsonData$results[c("name", "inspection_number","restaurant_id","type","date","score")],
+                       jsonData$results$address[c("street", "city","zip")])
+    #fix the dates
+    descPoint$date <- ymd(descPoint$date)
+    #fix the score
+    descPoint$score <-as.numeric(descPoint$score)
+    #fix the longitude and latitude
+    #change to numeric
+    descPoint$Longitude <- as.numeric(descPoint$Longitude)
+    descPoint$Latitude <- as.numeric(descPoint$Latitude)
+    #filter location errors
+    descPoint <- descPoint %>%
+      mutate(temp=floor(Longitude)) %>%
+      filter(temp==-123)
+    descPoint <- descPoint %>%
+      mutate(temp=floor(Latitude)) %>%
+      filter(temp==45)
     descPoint$content <- paste(sep = "<br/>",
                               descPoint$name, descPoint$score)
-    output$table = DT::renderDataTable(descPoint, selection = 'single')
+    rdescPoint <- descPoint %>%
+      filter(date>=ymd(as.character(Sys.Date()-365))) %>%
+      group_by(name, restaurant_id, street, zip) %>%
+      summarise(avg_score=mean(score))
+    mdescPoint <- descPoint %>%
+      group_by(Longitude, Latitude, name, restaurant_id) %>%
+      summarise(ave_score=mean(score))
+    mdescPoint$content <- paste(sep = "<br/>",
+                               mdescPoint$name, mdescPoint$ave_score)
+    output$table = DT::renderDataTable(rdescPoint<-dateNewData(), selection = 'single')
     output$foo = renderText(input$table_rows_selected)
     update_server <- reactive({
       input$fetch
+      browser()
       jsonstring<-paste("http://api.civicapps.org/restaurant-inspections?count=1000&since=", input$date2, sep="")
       jsonData <- fromJSON(as.character(jsonstring))
       descPoint <- cbind(jsonData$results$location[,c("Longitude", "Latitude")],
@@ -23,25 +51,56 @@ shinyServer(
       descPoint$content <- paste(sep = "<br/>",
                                descPoint$name, descPoint$score)
     })
-    output$startdate = renderText({
+    #makeReactiveBinding("rdescPoint")
+    #makeReactiveBinding("mdescPoint")
+       output$startdate = renderText({
       input$fetch
       isolate({
         #assemble string to fetch data
-        paste("http://api.civicapps.org/restaurant-inspections?count=1000&since=", input$date2, sep="")
+        as.character(input$date2)
       })
    })
+
+    dateNewData <- reactive({
+      input$fetch
+      isolate({
+        rdescPoint <- descPoint %>%
+          filter(date>=ymd(input$date2)) %>%
+          group_by(name, restaurant_id, street, zip) %>%
+          summarise(avg_score=mean(score))
+      })
+    })
+
+    computeDateScore <- eventReactive(input$fetch, {
+      isolate({
+        foo <- descPoint %>%
+          filter(date>=ymd(input$date2)) %>%
+          group_by(Longitude, Latitude, name, restaurant_id) %>%
+          summarise(ave_score=mean(score)) %>%
+          mutate(paste(sep = "<br/>", mdescPoint$name, mdescPoint$ave_score))
+        mdescPoint <- foo
+      })
+    })
     waitIcon <- makeIcon("Food-Waiter-icon.png",24,24)
     output$value <- renderPrint({ input$resName })
 
 
     output$mymap <- renderLeaflet({
-      leaflet(data=descPoint) %>% setView(lat = 45.5200, lng = -122.6819, zoom = 12) %>%
+      leaflet(mdescPoint) %>% setView(lat = 45.5200, lng = -122.6819, zoom = 12) %>%
         setMaxBounds(lng1=-122.2, lat1=45.4, lng2=-122.9, lat2=45.6) %>%
         addProviderTiles("CartoDB.Positron") %>%
-        addMarkers(~Longitude, ~Latitude, popup=~content,
+        addMarkers(data=computeDateScore(), ~Longitude, ~Latitude, popup=~content,
                    clusterOptions = markerClusterOptions(maxClusterRadius=80, disableClusteringAtZoom = 16),
                    icon=waitIcon)
      #update points of intrest when the map changes
     })
+
+    #observe({
+      #mdescPoint <- computeDateScore()
+     # leafletProxy("mymap") %>%
+      #  addMarkers(data=computeDateScore(), ~Longitude, ~Latitude, popup=~content,
+       #            clusterOptions = markerClusterOptions(maxClusterRadius=80, disableClusteringAtZoom = 16),
+        #           icon=waitIcon)
+    #})
     }
 )
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
   sidebarLayout( position = "left",
     sidebarPanel(
       h2("Overview"),
-      p("This application provides an interface to the restaurant inspection reports provided by the",a("civic apps API", href="http://api.civicapps.org/"), "in Portland Oregon"),
+      p("This application provides an interface to the restaurant inspection reports provided by the",a("civic apps API", href="http://api.civicapps.org/"), "in Portland Oregon. The date selection widget allows you to modify the start date for the data collection. The average inspection score for the time period is comp[uted and displayed in the table. Failing inspections are scored zero, so there is an adverse effect of failing inspections on the mean. The table is searchable and sortable to find your favorite resteraunts"),
       p("Restaurant Icon provided courstesy of",
         a("Icon8", href="http://icons8.com"),"."),
       br(),
@@ -17,33 +17,34 @@ shinyUI(fluidPage(
       span("RStudio", style = "color:blue")
     ),
     mainPanel(
-      fluidRow(
-        column(12,leafletOutput("mymap")),
-        column(1,
-               br()
-               )
-        ),
+#      could not get map updates working
+#      fluidRow(
+#        column(12,leafletOutput("mymap")),
+#        column(1,
+#               br()
+#               )
+#        ),
       # Copy the line below to make a text input box
       fluidRow(
         column(6,
                dateInput('date2',
-                         label = 'Start Date',
+                         label = h4('Start Date'),
                          value = as.character(Sys.Date()-365),
                          min = Sys.Date() - 1825, max = Sys.Date(),
                          format = "mm/dd/yy"
-                       ),
-               p("Current Values:", style = "color:#888888;"),
-               verbatimTextOutput("startdate")
+                       )
         ),
         column(6,
-               actionButton("fetch", label = "Fetch Data"),
-               p("Current Values:", style = "color:#888888;"),
-               verbatimTextOutput("resName")
+               p(h4("Data Since:"), style = "color:#888888;"),
+               verbatimTextOutput("startdate")
                )
       ),
       fluidRow(
         column(5,
-               verbatimTextOutput("foo")
+               actionButton("fetch", label = "Update Table")
+               ),
+        column(12,
+               br()
                )
       ),
       fluidRow(
